3faaa46f162e6b72dd6156a1250552d198194c27
Chris Dougherty <codehtmai@gmail.com>
2016-07-12 21:29:12 -0400

    Text and final-ish changes

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2016, 10, 22);
+	theday = new Date(2016, 9, 22);
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
 		 
